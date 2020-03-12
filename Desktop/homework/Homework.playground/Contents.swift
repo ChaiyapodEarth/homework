fa052c1de7cb19aa7b147508369efa9724e26935
Earth <earth@Earths-MacBook-Pro.local>
2020-03-12 12:46:02 +0700

    sum of method

---
+++ Desktop/homework/Homework.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var InputNamberOne:Int = 5
+var InputNumberTwo:Int = 4
+
+var Summary = InputNamberOne + InputNumberTwo
+var Different = InputNamberOne - InputNumberTwo
+var MUltiply = InputNamberOne * InputNumberTwo
+var Divide:Double = Double(InputNamberOne) / Double(InputNumberTwo)
+
+

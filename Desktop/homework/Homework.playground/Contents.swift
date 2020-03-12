4aeaeebbdd76be9245018ef704d4d8ada7b81819
Earth <earth@Earths-MacBook-Pro.local>
2020-03-12 13:01:01 +0700

    Calculate Money

--- Desktop/homework/Homework.playground/Contents.swift
+++ Desktop/homework/Homework.playground/Contents.swift
@@ -9,3 +9,17 @@ var MUltiply = InputNamberOne * InputNumberTwo
 var Divide:Double = Double(InputNamberOne) / Double(InputNumberTwo)
 
 
+
+let InputCurrency:Int = 5346
+var Bank1000:Int = InputCurrency/1000
+var Bank500:Int = (InputCurrency%1000)/500
+var Bank100:Int = ((InputCurrency%1000)%500)/100
+var Bank50:Int = (((InputCurrency%1000)%500)%100)/50
+var Bank20:Int = ((((InputCurrency%1000)%500)%100)%50)/20
+var Coin10:Int = (((((InputCurrency%1000)%500)%100)%50)%20)/10
+var Coin5:Int = ((((((InputCurrency%1000)%500)%100)%50)%20)%10)/50
+var Coin2:Int = (((((((InputCurrency%1000)%500)%100)%50)%20)%10)%5)/2
+var Coin1:Int = ((((((((InputCurrency%1000)%500)%100)%50)%20)%10)%5)%2)/1
+
+
+
